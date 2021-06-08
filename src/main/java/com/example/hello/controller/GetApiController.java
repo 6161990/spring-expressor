6a48e5fdf08cfc37aedc051dacc43f5189afac41
6161990 <6161990src@gmail.com>
2021-06-08 13:46:22 +0900

    Get Api 2

--- src/main/java/com/example/hello/controller/GetApiController.java
+++ src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UesrRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -30,4 +33,66 @@ public class GetApiController {
         System.out.println("PathVariable : "+name);
         return name;
     }
+
+    //but, /path-variable/{name}과 @PathVariable String name 의 이름이 다를 때 !!!(이름을 일치 시켜줄 수 없을 때)
+    @GetMapping("/path-variable2/{name}")
+    public String pathVariable2(@PathVariable(name = "name") String pathName){
+        System.out.println("PathVariable : "+pathName);
+        return pathName;
+    }
+
+    // what is queryParameter ?
+    // search?q=%EC%9D%B8%ED%85%94%EB%A6%AC%EC%A0%9C%EC%9D%B4
+    // &oq=%EC%9D%B8%ED%85%94%EB%A6%AC%EC%A0%9C%EC%9D%B4
+    // &aqs=chrome..69i57j69i59l6j69i61.1173j0j15
+    // &sourceid=chrome
+    // &ie=UTF-8
+    // => ?key=value
+    //    &key2=value2 : 물음표로 시작해서 key = value 이런식의 구성으로 되어있따.
+
+    //http://localhost:9090/api/get/query-param?user=yoonji&email=jineon@gamil.com&age=25
+    @GetMapping(path = "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam){
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach( entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey()+"="+entry.getValue()+"\n");
+        });
+        return sb.toString();
+    }
+
+    //queryParam을 Map으로 받는 경우에는 key를 알 수가 없음
+    //key를 user, email, age로 명확하게 지정을 한 경우에는 어떻게 해야할까?
+    @GetMapping("query-param2")
+    public String queryParam2(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+    ){
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name+" "+email+" "+age;
+    }
+    // 하지만, 모든 파라미터마다  @RequestParam 하므로 이 방식은 비효율적.
+    // so, DTO를 이용하는 것이 바람직.
+
+
+    @GetMapping("query-param3")
+    public String queryParam3(UesrRequest userRequest){
+        //@RequestParam 을 붙여주지않고, 객체를 전달해주면 물음표 뒤에 있는 값들은 스프링 부트에서 판단해준다.
+        //?user=yoonji&email=jineon@gamil.com&age=25   에서
+        //key(user, email, age)에 해당하는 것들을 해당객체(DTO)에서 찾아 변수 매칭을 해준다.
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+    }
+
 }
