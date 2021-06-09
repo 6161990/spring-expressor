e37054edf4bcb7fc8bd823eec57cf03fd3c32e7c
6161990 <6161990src@gmail.com>
2021-06-09 13:50:52 +0900

    Delete Api

---
+++ src/main/java/com/example/hello/controller/DeleteController.java
@@ -0,0 +1,15 @@
+package com.example.hello.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/delete-api")
+public class DeleteController {
+
+    @DeleteMapping("/delete/{userId}")
+    public void delete(@PathVariable String userId, @RequestParam String account){
+        System.out.println(userId);
+        System.out.println(account);
+    }
+}
