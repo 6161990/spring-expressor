33661d711d1af97e01b8688cefc04b21b3267b26
6161990 <6161990src@gmail.com>
2021-06-16 16:15:03 +0900

    Custom Validation
    1. AssertTrue/False method 이용(재사용 불가)
    2. Constraint Validator 적용해 재사용
    
    //User의 몇몇 필드, get,set,toString(일부) 주석은 객체 내 클래스 사용시(List<Car>) valid 사용법 설명을 위한 것.

--- Vaildation/src/main/java/com/example/vaildation/dto/User.java
+++ Vaildation/src/main/java/com/example/vaildation/dto/User.java
@@ -1,9 +1,12 @@
 package com.example.vaildation.dto;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import com.example.vaildation.annotaion.YearMonth;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class User {
 
@@ -13,11 +16,18 @@ public class User {
     @Max(value = 90)
     private int age;
 
-    @Email
-    private String email;
+//    @Email
+//    private String email;
+//
+//    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$")
+//    private String phoneNumber;
 
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$")
-    private String phoneNumber;
+//    @YearMonth(pattern = "yyyyMM")
+//    private String reqYearMonth;  //yyyyMM
+
+    @Valid
+    private List<Car> cars;
+
 
     public String getName() {
         return name;
@@ -35,29 +45,60 @@ public class User {
         this.age = age;
     }
 
-    public String getEmail() {
-        return email;
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public String getReqYearMonth() {
+//        return reqYearMonth;
+//    }
+
+//    public void setReqYearMonth(String reqYearMonth) {
+//        this.reqYearMonth = reqYearMonth;
+//    }
+
+
+    public List<Car> getCars() {
+        return cars;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+    //  재사용 불가 Annotation 만들어 주고 Validator 클래스 생성하기
+//    public boolean isReqYearMonthValidation(){
+//        System.out.println("assert true call");
+//        try{
+//            LocalDate localDate = LocalDate.parse(getReqYearMonth()+"01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+//        }catch (Exception e){
+//            return false;
+//        }
+//        return true;
+//    }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 
     @Override
     public String toString() {
         return "User{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
-                ", email='" + email + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
+    //             ", email='" + email + '\'' +
+    //            ", phoneNumber='" + phoneNumber + '\'' +
+    //            ", reqYearMonth='" + reqYearMonth + '\'' +
+                ", cars=" + cars +
                 '}';
     }
 }
