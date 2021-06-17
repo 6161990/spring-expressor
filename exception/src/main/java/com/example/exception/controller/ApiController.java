fda3fbc34df87ade430bd3e8d8e7f3dda7952a19
6161990 <6161990src@gmail.com>
2021-06-17 12:45:58 +0900

    validation 전역처리, 특정 api 지정 처리

---
+++ exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -0,0 +1,25 @@
+package com.example.exception.controller;
+
+import com.example.exception.dto.User;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/user")
+public class ApiController {
+
+    @GetMapping("")
+    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+
+        return user;
+    }
+
+    @PostMapping("")
+    public User post(@Valid @RequestBody User user){
+        return user;
+    }
+}
