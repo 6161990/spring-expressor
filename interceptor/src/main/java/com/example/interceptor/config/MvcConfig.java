345449a1923d4b423be11b6982de9e2f2f9ef856
6161990 <6161990src@gmail.com>
2021-06-18 18:40:38 +0900

    Interception
    권한 check

---
+++ interceptor/src/main/java/com/example/interceptor/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.interceptor.config;
+
+import com.example.interceptor.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class MvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/api/private/*");
+        //registry.addInterceptor()
+        //이렇게 뒤에 계속 선언하면, 인증의 과정을 두고 depth 로 하나씩 거치게 되어있음.
+    }
+}
