87cafd1f1731da84e95ef3d228787b9777b7f1c8
6161990 <6161990src@gmail.com>
2021-07-23 12:26:17 +0900

    웹 스코프
    request 스코프, provider, proxy

---
+++ shop/src/main/java/com/company/shop/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package com.company.shop.web;
+
+import com.company.shop.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+        String requestURL = request.getRequestURI().toString();
+       // MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("myLogger = "+ myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
