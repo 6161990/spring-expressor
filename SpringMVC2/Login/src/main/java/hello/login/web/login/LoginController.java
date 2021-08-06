07c5899d9ca0752f6ac737aa016278553177d79b
6161990 <6161990src@gmail.com>
2021-08-06 18:03:03 +0900

    로그인 처리 2 - 필터, 인터셉터
    * 서블릿 필터 - 요청 로그
    * 서블릿 필터 - 인증 체크
    * 스프링 인터셉터 - 요청로그
    * 스프링 인터셉터 - 인증 체크
    * ArgumentResolver

--- SpringMVC2/Login/src/main/java/hello/login/web/login/LoginController.java
+++ SpringMVC2/Login/src/main/java/hello/login/web/login/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -102,7 +103,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV3(@Validated @ModelAttribute LoginForm loginForm,
                           BindingResult bindingResult,
                           HttpServletRequest request){
@@ -135,6 +136,37 @@ public class LoginController {
             session.invalidate();
         }
         return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV4(@Validated @ModelAttribute LoginForm form,
+                          BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL,
+                          HttpServletRequest request){
+
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("login? {}", loginMember);
+
+        if(loginMember == null){
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리
+
+        //세셩이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        //redirectURL 적용
+        return "redirect:" + redirectURL;
+
     }
 
 
