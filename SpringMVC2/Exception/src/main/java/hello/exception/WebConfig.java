4713884732c93355032af52adf5f5f61363831a2
6161990 <6161990src@gmail.com>
2021-08-09 11:59:45 +0900

    API 예외처리
    * HandlerExceptionResolver 활용
    * 스프링의 ExceptionResolver
    * @ExceptionHandler
    * @ControllerAdvice

--- SpringMVC2/Exception/src/main/java/hello/exception/WebConfig.java
+++ SpringMVC2/Exception/src/main/java/hello/exception/WebConfig.java
@@ -3,8 +3,8 @@ package hello.exception;
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
 import hello.exception.resolver.MyHandlerExceptionResolver;
+import hello.exception.resolver.UserHandlerExceptionResolver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -28,6 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
         resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
     }
 
     //    @Bean
