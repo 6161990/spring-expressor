af9878385f5b1235e5b8266f8e6eef1c13bc8f05
6161990 <6161990src@gmail.com>
2021-08-10 14:53:36 +0900

    파일업로드, 다운로드

---
+++ SpringMVC2/Upload/src/main/java/hello/upload/domain/Item.java
@@ -0,0 +1,16 @@
+package hello.upload.domain;
+
+
+import hello.upload.domain.UploadFile;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private UploadFile attachFile;
+    private List<UploadFile> imageFiles;
+}
