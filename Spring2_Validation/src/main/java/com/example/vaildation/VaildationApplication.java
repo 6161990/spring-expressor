bb63e33c2ed538043119c81d7a60467762cedb3e
6161990src <6161990src@gmail.com>
2022-01-28 21:39:56 +0900

    파일명 변경

---
+++ Spring2_Validation/src/main/java/com/example/vaildation/VaildationApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaildation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaildationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaildationApplication.class, args);
+    }
+
+}
---
+++ Spring2_Validation/src/test/java/com/example/vaildation/VaildationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaildation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaildationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
