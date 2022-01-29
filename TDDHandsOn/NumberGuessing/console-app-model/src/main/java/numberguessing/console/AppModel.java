3580aff5062dfe7bf4cd54115d5d590cedaa4d31
6161990src <6161990src@gmail.com>
2022-01-29 09:30:18 +0900

    sut_correctly_prints_too_low_message_in_single_player_game 테스트 케이스에 맞는 코드 작성

--- TDDHandsOn/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ TDDHandsOn/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -6,11 +6,13 @@ public final class AppModel {
     private static final String NEW_LINE = System.lineSeparator();
     boolean completed;
     private String output;
+    private int answer;
 
     public AppModel(PositiveIntegerGenerator generator) {
         completed = false;
         output = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
                 "3: Exit" + NEW_LINE + "Enter selection: ";
+        answer = generator.generateLessThanOrEqualToHundread();
     }
 
     public boolean isCompleted() {
@@ -25,9 +27,14 @@ public final class AppModel {
         if(input.equals("1")) {
             output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
                     + NEW_LINE + "Enter your guess: ";
+            return;
         } else if(input.equals("3")){
             completed = true;
-        } else if(input.equals("40")){
+            return;
+        }
+
+        int guess = Integer.parseInt(input);
+        if(guess < answer){
             output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
         }
     }
