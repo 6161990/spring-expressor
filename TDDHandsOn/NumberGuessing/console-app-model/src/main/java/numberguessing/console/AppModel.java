64327ea590641866606bb9358203abb4bebb9fe9
6161990src <6161990src@gmail.com>
2022-01-31 12:27:32 +0900

    플레이어모드와 게임모드 분리

--- TDDHandsOn/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ TDDHandsOn/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -7,12 +7,14 @@ public final class AppModel {
     boolean completed;
     private String output;
     private int answer;
+    private boolean singlePlayerMode;
 
     public AppModel(PositiveIntegerGenerator generator) {
         completed = false;
         output = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
                 "3: Exit" + NEW_LINE + "Enter selection: ";
         answer = generator.generateLessThanOrEqualToHundread();
+        singlePlayerMode = false;
     }
 
     public boolean isCompleted() {
@@ -23,23 +25,26 @@ public final class AppModel {
         return output;
     }
 
-    public void processInput(String input) { // 상태변화를 관리하는 방법이 필요함. 게임 모드를 선택하는 건지, 플레이어가 진행중인지 분리할 필요가 있음.
-        if(input.equals("1")) {
-            output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
-                    + NEW_LINE + "Enter your guess: ";
-            return;
-        } else if(input.equals("3")){
-            completed = true;
-            return;
-        }
-
-        int guess = Integer.parseInt(input);
-        if(guess < answer){
-            output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
-        } else if (guess > answer) {
-            output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
-        } else if (guess == answer) {
-            output = "Correct! ";
+    public void processInput(String input) {
+        if(singlePlayerMode){
+            int guess = Integer.parseInt(input);
+            if(guess < answer){
+                output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
+            } else if (guess > answer) {
+                output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
+            } else if (guess == answer) {
+                output = "Correct! ";
+            }
+        } else {
+            if(input.equals("1")) {
+                output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
+                        + NEW_LINE + "Enter your guess: ";
+                singlePlayerMode = true;
+                return;
+            } else if(input.equals("3")){
+                completed = true;
+                return;
+            }
         }
     }
 }
