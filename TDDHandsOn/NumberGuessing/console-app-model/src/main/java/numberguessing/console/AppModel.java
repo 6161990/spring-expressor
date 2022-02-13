2feb44b0a5c5d57a3ffe573e93a7755e7e2a7a4a
6161990src <6161990src@gmail.com>
2022-02-13 15:33:41 +0900

    루프가 한번 돌 때마다 프로세서를 한번 교체해주는 방식으로 refactoring : 두 번째 단계, 프로세서 실행하고 교체시키는 processInput()

--- TDDHandsOn/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ TDDHandsOn/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -17,6 +17,7 @@ public final class AppModel {
     private int answer;
     private boolean singlePlayerMode;
     private int tries;
+    private Processor processor;
 
     public AppModel(PositiveIntegerGenerator generator) {
         this.generator = generator;
@@ -24,6 +25,7 @@ public final class AppModel {
         output = SELECT_MODE;
         singlePlayerMode = false;
         tries = 0;
+        processor = this::processModelSelection;
     }
 
     public boolean isCompleted() {
@@ -34,12 +36,20 @@ public final class AppModel {
         return output;
     }
 
-    public void processInput(String input) {
-        if(singlePlayerMode){
-            processSinglePlayerGame(input);
+    public void processInput(String input) { // 프로세서를 실행하고 결과값으로 프로세스를 다시 교체하는 역할로 재구현
+        processor = processor.run(input);
+    }
+
+    private Processor processModelSelection(String input) {
+        if(input.equals("1")) {
+            output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
+                    + NEW_LINE + "Enter your guess: ";
+            singlePlayerMode = true;
+            answer = generator.generateLessThanOrEqualToHundred();
         } else {
-            processModelSelection(input);
+            completed = true;
         }
+        return null;
     }
 
     private void processSinglePlayerGame(String input) {
@@ -55,14 +65,4 @@ public final class AppModel {
         }
     }
 
-    private void processModelSelection(String input) {
-        if(input.equals("1")) {
-            output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
-                    + NEW_LINE + "Enter your guess: ";
-            singlePlayerMode = true;
-            answer = generator.generateLessThanOrEqualToHundred();
-        } else {
-            completed = true;
-        }
-    }
 }
