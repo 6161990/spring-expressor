ced687e37edc1f0c90e6b4e1a3f5feaeb3756f31
6161990src <6161990src@gmail.com>
2022-02-14 08:45:18 +0900

    두 번째 플레이어의 입력을 알리는 출력문까지 제대로 나오게 수정 + 플레이어이름 담는 배열문 분해 수정

--- TDDHandsOn/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ TDDHandsOn/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -2,6 +2,8 @@ package numberguessing.console;
 
 import numberguessing.PositiveIntegerGenerator;
 
+import java.util.stream.Stream;
+
 public final class AppModel {
     private static final String NEW_LINE = System.lineSeparator();
     private static final String SELECT_MODE = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
@@ -52,9 +54,12 @@ public final class AppModel {
 
     private Processor getMultiPlayerGameProcessor() {
         return input -> {
-            String[] players = input.split(",");
+            Object[] players = Stream.of(input.split(",")).map(String::trim).toArray(); // 플레이어 이름 옆에 공백이 하나 더 들어가서 처리(CsvSource 에 들어간 배열값을 자세히보라), toArray의 반환타입이 Object이기 때문에 반환타입 수정.
             output = "I'm thinking of a number between 1 and 100." + "Enter " + players[0] + "'s guess: ";
-            return input2 -> null;
+            return input2 -> {
+                output = "Enter " + players[1] + "'s guess: ";
+                return null;
+            };
         };
     }
 
