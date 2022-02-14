6f0766db11f3e7349cd42129fb67d6c3a3aa7059
6161990src <6161990src@gmail.com>
2022-02-15 08:12:25 +0900

    String output 메세지를 StringBuffer ouputBuffer로 변경. 기존엔 모든 메세지 출력이 대입.초기화(=, +=)였다면 이젠 추가로 바뀌게됨(append()). -> 메세지 출력문들이 리팩토링 하기 좋게 다 같은 모양새로 바뀜.

--- TDDHandsOn/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ TDDHandsOn/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 
 public final class AppModel {
     private static final String NEW_LINE = System.lineSeparator();
-    private static final String SELECT_MODE = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
+    private static final String SELECT_MODE_MESSAGE = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
             "3: Exit" + NEW_LINE + "Enter selection: ";
 
     interface Processor {
@@ -14,14 +14,14 @@ public final class AppModel {
     }
 
     private final PositiveIntegerGenerator generator;
+    private final StringBuffer outputBuffer;
     boolean completed;
-    private String output;
     private Processor processor;
 
     public AppModel(PositiveIntegerGenerator generator) {
         this.generator = generator;
         completed = false;
-        output = SELECT_MODE;
+        outputBuffer = new StringBuffer(SELECT_MODE_MESSAGE); // 초기화
         processor = this::processModeSelection;
     }
 
@@ -30,7 +30,9 @@ public final class AppModel {
     }
 
     public String flushOutput() {
-        return output;
+        String flushOutput = outputBuffer.toString();
+        outputBuffer.setLength(0); // 버퍼 비워주기
+        return flushOutput;
     }
 
     public void processInput(String input) { // 프로세서를 실행하고 결과값으로 프로세스를 다시 교체하는 역할로 재구현
@@ -39,12 +41,12 @@ public final class AppModel {
 
     private Processor processModeSelection(String input) {
         if(input.equals("1")) {
-            output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
-                    + NEW_LINE + "Enter your guess: ";
+            outputBuffer.append("Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
+                    + NEW_LINE + "Enter your guess: ");
             int answer = generator.generateLessThanOrEqualToHundred();
             return getSinglePlayerGameProcessor(answer, 1);
         } else if(input.equals("2")) {
-            output = "Multiplayer game" + NEW_LINE + "Enter player names separated with commas: ";
+            outputBuffer.append("Multiplayer game" + NEW_LINE + "Enter player names separated with commas: ");
             return getMultiPlayerGameProcessor();
         } else {
             completed = true;
@@ -55,11 +57,12 @@ public final class AppModel {
     private Processor getMultiPlayerGameProcessor() {
         return input -> {
             Object[] players = Stream.of(input.split(",")).map(String::trim).toArray(); // 플레이어 이름 옆에 공백이 하나 더 들어가서 처리(CsvSource 에 들어간 배열값을 자세히보라), toArray의 반환타입이 Object이기 때문에 반환타입 수정.
-            output = "I'm thinking of a number between 1 and 100." + "Enter " + players[0] + "'s guess: ";
+            outputBuffer.append("I'm thinking of a number between 1 and 100.");
+            outputBuffer.append("Enter " + players[0] + "'s guess: ");
             return input2 -> {
-                output = "Enter " + players[1] + "'s guess: ";
+                outputBuffer.append("Enter " + players[1] + "'s guess: ");
                 return input3 -> {
-                    output = "Enter " + players[2] + "'s guess: ";
+                    outputBuffer.append("Enter " + players[2] + "'s guess: ");
                     return null;
                 };
             };
@@ -70,13 +73,13 @@ public final class AppModel {
         return input -> {
             int guess = Integer.parseInt(input);
             if (guess < answer) {
-                output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
+                outputBuffer.append("Your guess is too low." + NEW_LINE + "Enter your guess: ");
                 return getSinglePlayerGameProcessor(answer, tries + 1);
             } else if (guess > answer) {
-                output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
+                outputBuffer.append("Your guess is too high." + NEW_LINE + "Enter your guess: ");
                 return getSinglePlayerGameProcessor(answer, tries + 1);
             } else {
-                output = "Correct! " + tries + (tries == 1 ? " guess." : " guesses.") + NEW_LINE + SELECT_MODE;
+                outputBuffer.append("Correct! " + tries + (tries == 1 ? " guess." : " guesses.") + NEW_LINE + SELECT_MODE_MESSAGE);
                 return this::processModeSelection;
             }
         };
