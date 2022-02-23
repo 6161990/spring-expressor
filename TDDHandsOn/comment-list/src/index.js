b19dbc1fca5afab4954f63ec8be8c8f75c187d38
Yoonji Kim <6161990src@gmail.com>
2022-02-24 08:35:29 +0900

    default setting

---
+++ TDDHandsOn/comment-list/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import commentComposerFactory from "./commentComposerFactory";
+import commentRefiner from "./content-refiners/trimWhitespaces";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App commentComposer={commentComposerFactory(commentRefiner)} />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
