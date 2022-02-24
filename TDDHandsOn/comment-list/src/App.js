9508cb0906b23d3f85931a5115352bb0e108516b
Yoonji Kim <6161990src@gmail.com>
2022-02-25 06:14:22 +0900

    App 에서 Form 을 외부화- 앱 내부에서 정의된 코드를 주입받고 잇기 때문에 외부화 가능

--- TDDHandsOn/comment-list/src/App.js
+++ TDDHandsOn/comment-list/src/App.js
@@ -15,39 +15,38 @@ function App({ commentComposer }) {
       <CommentList comments={comments}/>
     </div>
   );
+}
 
-  function Form({ commentComposer }) {
-    const [author, setAuthor] = useState("");
-    const [content, setContent] = useState("");
+function Form({ commentComposer }) {
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
 
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const newComment = commentComposer({ author, content }); // form 외부에서 전달된 것
-      onNewComment(newComment); // form 외부에서 전달된 것
-      setAuthor("");
-      setContent("");
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newComment = commentComposer({ author, content }); // form 외부에서 전달된 것
+    onNewComment(newComment); // form 외부에서 전달된 것
+    setAuthor("");
+    setContent("");
+  };
 
-    return <form onSubmit={handleSubmit}>
-      <input
-          name="author"
-          value={author}
-          type="text"
-          onChange={(e) => setAuthor(e.currentTarget.value)}
-          placeholder="작성자"
-      />
-      <input
-          name="content"
-          value={content}
-          type="text"
-          onChange={(e) => setContent(e.currentTarget.value)}
-          placeholder="내용"
-      />
-      <button name="submit">입력</button>
-    </form>;
-  }
-
+  return <form onSubmit={handleSubmit}>
+    <input
+        name="author"
+        value={author}
+        type="text"
+        onChange={(e) => setAuthor(e.currentTarget.value)}
+        placeholder="작성자"
+    />
+    <input
+        name="content"
+        value={content}
+        type="text"
+        onChange={(e) => setContent(e.currentTarget.value)}
+        placeholder="내용"
+    />
+    <button name="submit">입력</button>
+  </form>;
 }
 
 export default App;
