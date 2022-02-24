6859db712496c85c2c1b5c70e236cc1ab1a1af1d
Yoonji Kim <6161990src@gmail.com>
2022-02-24 09:03:24 +0900

    새로운 요구사항 : Composite 패턴
     요구사항이 바뀌어서 앞뒤 공백을 절삭(1번 기능) 하고 모든 중복된 공백을 하나로 줄이는(2번 기능)이 모두 있었으면 좋겠다라고 한다면?
    계속 변화되는 환경에 어떻게 적응하는가. OCP와 테스트 용이성과 적응력

--- TDDHandsOn/comment-list/src/index.js
+++ TDDHandsOn/comment-list/src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import compositeContentRefinerFactory from "./content-refiners/compositeContentRefinerFactory";
+import compactWhitespace from "./content-refiners/compactWhitespace";
+import trimWhitespaces from "./content-refiners/trimWhitespaces";
 import commentComposerFactory from "./commentComposerFactory";
-import commentRefiner from "./content-refiners/compactWhitespace";
 
+const commentRefiner = compositeContentRefinerFactory([
+    compactWhitespace,
+    trimWhitespaces,
+]);
 ReactDOM.render(
   <React.StrictMode>
     <App commentComposer={commentComposerFactory(commentRefiner)} />
