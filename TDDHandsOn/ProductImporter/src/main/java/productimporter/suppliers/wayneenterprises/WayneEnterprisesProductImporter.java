e44b71c8c7b6e9c29226495ad931707e0462a49b
Yoonji Kim <6161990src@gmail.com>
2022-02-28 17:57:53 +0900

    테스트 설명과 비즈니스 로직 수정 For Success

--- TDDHandsOn/ProductImporter/src/main/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter.java
+++ TDDHandsOn/ProductImporter/src/main/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter.java
@@ -1,18 +1,24 @@
 package productimporter.suppliers.wayneenterprises;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import productimporter.Product;
 import productimporter.ProductImporter;
 
 public final class WayneEnterprisesProductImporter implements ProductImporter {
 
+    private final WayneEnterprisesProductSource dataSource;
+
     public WayneEnterprisesProductImporter(WayneEnterprisesProductSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Override
     public Iterable<Product> fetchProducts() {
-        return new ArrayList<Product>();
+        ArrayList<WayneEnterprisesProduct> products = new ArrayList<>();
+        dataSource.fetchProducts().forEach(products::add);
+        return Arrays.asList(new Product[products.size()]);
     }
 
 }
--- TDDHandsOn/ProductImporter/src/test/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter_specs.java
+++ TDDHandsOn/ProductImporter/src/test/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter_specs.java
@@ -8,11 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WayneEnterprisesProductImporter_specs {
 
+    // Wayne 사의 모든 상품수(ProductSource)를 반환하면 그 상품 갯수 만큼, 우리 commercial 가 규정하고 있는 상품 포맷에 맞는 상품들이 반환되어야 함을 테스트
     @ParameterizedTest
-    @DomainArgumentsSource
+    @DomainArgumentsSource // 직접 만든 어노테이션
     void sut_projects_all_products(WayneEnterprisesProduct[] source) {
         var stub = new WayneEnterprisesProductSourceStub(source);
-        var sut = new WayneEnterprisesProductImporter(stub);
+        var sut = new WayneEnterprisesProductImporter(stub);  // 테스트 메소드 파라미터로 입력받은 source 그대로를 테스트 대상 sut에 제공하도록 간접 입력
 
         Iterable<Product> actual = sut.fetchProducts();
         assertThat(actual).hasSize(source.length);
