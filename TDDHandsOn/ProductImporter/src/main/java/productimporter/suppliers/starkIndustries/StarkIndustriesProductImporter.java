6bf73d71c6cab8248c72e995cf8e9dc784081bd0
Yoonji Kim <6161990src@gmail.com>
2022-03-03 19:17:32 +0900

    모든 상품 개수를 확인하는 테스트를 위한 코드 로직 수정

--- TDDHandsOn/ProductImporter/src/main/java/productimporter/suppliers/starkIndustries/StarkIndustriesProductImporter.java
+++ TDDHandsOn/ProductImporter/src/main/java/productimporter/suppliers/starkIndustries/StarkIndustriesProductImporter.java
@@ -3,14 +3,24 @@ package productimporter.suppliers.starkIndustries;
 import productimporter.Product;
 import productimporter.ProductImporter;
 
+import java.util.ArrayList;
+
 public class StarkIndustriesProductImporter implements ProductImporter {
+
+    private final StarkIndustriesProductSource productSource;
 
     public StarkIndustriesProductImporter(StarkIndustriesProductSource productSource,
                                           StarkIndustriesProductTranslator translator) {
+        this.productSource = productSource;
     }
 
     @Override
     public Iterable<Product> fetchProducts() {
-        return null;
+        Iterable<StarkIndustriesProduct> source = productSource.getAllProducts();
+        var products = new ArrayList<Product>();
+        for (StarkIndustriesProduct s : source) {
+            products.add(null);
+        }
+        return products;
     }
 }
