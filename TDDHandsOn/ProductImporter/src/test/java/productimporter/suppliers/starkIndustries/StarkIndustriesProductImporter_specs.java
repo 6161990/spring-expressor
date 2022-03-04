d8bc04329c86b4c8be0ba7138307bc7ba4d23a95
Yoonji Kim <6161990src@gmail.com>
2022-03-04 20:39:36 +0900

    starkIndustries 상품을 도메인에 맞게 잘 변형되는지 확인하는 테스트 -> WayneEnterprises 의 sut_correctly_projects_source_properties 테스트케이스와 비교

--- TDDHandsOn/ProductImporter/src/test/java/productimporter/suppliers/starkIndustries/StarkIndustriesProductImporter_specs.java
+++ TDDHandsOn/ProductImporter/src/test/java/productimporter/suppliers/starkIndustries/StarkIndustriesProductImporter_specs.java
@@ -1,10 +1,14 @@
 package productimporter.suppliers.starkIndustries;
 
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.params.ParameterizedTest;
 import productimporter.DomainArgumentsSource;
 import productimporter.Product;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -27,5 +31,33 @@ public class StarkIndustriesProductImporter_specs {
 
         //Assert
         assertThat(actual).hasSize(sourceProducts.length);
+    }
+
+    // starkIndustries 상품을 도메인에 맞게 잘 변형되는지 확인하는 테스트
+    @ParameterizedTest
+    @DomainArgumentsSource
+    void sut_correctly_translates_source_products(StarkIndustriesProduct[] sourceProducts, Product[] product) {
+        //Arrange
+        var productSource = mock(StarkIndustriesProductSource.class);
+        when(productSource.getAllProducts()).thenReturn(Arrays.asList(sourceProducts));
+
+        var translator = mock(StarkIndustriesProductTranslator.class);
+
+        List<Tuple> tuples = IntStream.range(0, Math.min(sourceProducts.length, product.length))
+                .mapToObj(index -> Tuple.tuple(sourceProducts[index], product[index])).collect(Collectors.toList());
+
+        for(Tuple tuple : tuples){
+            Object[] values = tuple.toArray();
+            when(translator.translate((StarkIndustriesProduct) values[0])).thenReturn((Product) values[1]);
+        }
+
+        var sut = new StarkIndustriesProductImporter(productSource, translator);
+
+        //Act
+        Iterable<Product> actual = sut.fetchProducts();
+
+        //Assert
+        assertThat(actual).containsExactly(product);
+
     }
 }
