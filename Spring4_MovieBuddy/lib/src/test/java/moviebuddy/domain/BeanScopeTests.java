22fb34c0a297aeaa3de79ba94b62c75f6f846278
Yoonji Kim <6161990src@gmail.com>
2022-03-06 11:26:08 +0900

    빈의 싱글톤 스코프 테스트

---
+++ Spring4_MovieBuddy/lib/src/test/java/moviebuddy/domain/BeanScopeTests.java
@@ -0,0 +1,24 @@
+package moviebuddy.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import moviebuddy.MovieBuddyFactory;
+
+public class BeanScopeTests {
+
+	@Test
+	void Equals_MovieFinderBean() { // 싱글톤 스코프
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
+		MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
+		
+		Assertions.assertEquals(movieFinder, applicationContext.getBean(MovieFinder.class));
+	}
+	
+	@Test
+	void NotEquals_MovieFinderBean() {
+		
+	}
+}
