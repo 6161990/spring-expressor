30f777fd7f1ef96edf73d366c6d9f0e8bbdafa83
Yoonji Kim <6161990src@gmail.com>
2022-03-06 11:12:50 +0900

    스프링 구성 정보를 읽어들여 어떻게 빈을 생성할지 결정하는 방법 2가지

--- Spring4_MovieBuddy/lib/src/main/java/moviebuddy/MovieBuddyFactory.java
+++ Spring4_MovieBuddy/lib/src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -5,13 +5,30 @@ import org.springframework.context.annotation.Configuration;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
 
 @Configuration // 빈 구성정보 - Configuration 메타데이터로 사용함을 선언 
 public class MovieBuddyFactory { //객체를 생성하고 구성하는 역할 
 	
+	@Bean
+	public MovieReader movieReader() { // 등록된 또 하나의 빈 
+		return new CsvMovieReader();
+	}
+	
 	@Bean // 어떤 빈이 있는가 
 	public MovieFinder movieFinder() {
-		return new MovieFinder(new CsvMovieReader());
+		return new MovieFinder(movieReader()); // 메소드 호출 방식 
 	}
 
+	/**
+	 * 
+	 *  @Bean  
+		public MovieFinder movieFinder(MovieReader movieReader) {
+			return new MovieFinder(movieReader);  
+		}
+		 파라미터 호출 방식 :  MovieFinder 빈을 등록하려고 할 때,
+		 * 메서드의 파라미를 보고 MovieFinder을 등록하려면 MovieReader가 필요하겠구나 하고 
+		 * 스스로 내부에 MovieReader빈이 있는지 확인한다.
+	 * 
+	 */
 }
