f053d153ed03079791f741df270242746e91fc20
Yoonji Kim <6161990src@gmail.com>
2022-03-10 10:06:32 +0900

    setter 이용한 의존성 주입

--- Spring4_MovieBuddy/lib/src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ Spring4_MovieBuddy/lib/src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -15,10 +15,15 @@ import moviebuddy.MovieBuddyFactory;
 @ContextConfiguration(classes=MovieBuddyFactory.class) // 하위에 있던 애플리케이션 컨텍스트 생성을없애줘도된다. 이제 테스트도 스프링컨텍스트를 이용하니까! 
 public class MovieFinderTest { 
 			
-	final MovieFinder movieFinder;
+	MovieFinder movieFinder;
+	
+//	@Autowired
+//	MovieFinderTest(MovieFinder movieFinder){
+//		this.movieFinder = movieFinder;
+//	}
 	
 	@Autowired
-	MovieFinderTest(MovieFinder movieFinder){
+	void setMovieFinder(MovieFinder movieFinder){
 		this.movieFinder = movieFinder;
 	}
 
