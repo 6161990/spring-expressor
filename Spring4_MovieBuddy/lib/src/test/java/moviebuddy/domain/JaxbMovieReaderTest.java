04ddfdb3fe1e773f442a3538fa7384f1ed46b534
Yoonji Kim <6161990src@gmail.com>
2022-03-10 11:48:26 +0900

    JaxbMovieReaderTest 도  스프링 테스트 컨텍스트 프레임워크 사용하는 것으로 변경한다.

--- Spring4_MovieBuddy/lib/src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
+++ Spring4_MovieBuddy/lib/src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -4,13 +4,19 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import moviebuddy.MovieBuddyFactory;
+
+@SpringJUnitConfig(MovieBuddyFactory.class) // 위 둘을 한꺼번에 해결해주는 마법사 
 public class JaxbMovieReaderTest {
+	
+	@Autowired
+	JaxbMovieReader movieReader;  //구체적인 타입으로 의존 관계로 주입받음. MovieReader로 받으면 동일한 타입의 빈이 있어서 테스트 실
 	
 	@Test
 	void NotEmpty_LoadedMovies() {
-		JaxbMovieReader movieReader = new JaxbMovieReader();
-		
 		List<Movie> movies = movieReader.loadMovies();
 		Assertions.assertEquals(1375, movies.size());
 	}
