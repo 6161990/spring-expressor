631c1eba37a38cb2fb5aa2634eb98d47c15e235f
Yoonji Kim <6161990src@gmail.com>
2022-03-19 14:52:01 +0900

    T init

---
+++ Design Pattern/src/com/company/design/specification/ScheduleTriggerRuleMatchCondition.java
@@ -0,0 +1,7 @@
+package com.company.design.specification;
+
+public class ScheduleTriggerRuleMatchCondition {
+    public boolean isSatisfy(Object o){
+        return false;
+    }
+}
---
+++ Design Pattern/src/com/company/design/specification/Specification.java
@@ -0,0 +1,4 @@
+package com.company.design.specification;
+
+public class Specification {
+}
