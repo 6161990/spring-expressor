dde0f5a4ff9a34377dc8bafa3580da7935f8aa87
Yoonji Kim <6161990src@gmail.com>
2022-03-19 16:38:50 +0900

    add DisplayName

--- T/src/test/java/com/yoon/t/specification/ScheduleTriggerRuleMatchConditionTest.java
+++ T/src/test/java/com/yoon/t/specification/ScheduleTriggerRuleMatchConditionTest.java
@@ -1,6 +1,7 @@
 package com.yoon.t.specification;
 
 import org.assertj.core.util.Maps;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ScheduleTriggerRuleMatchConditionTest {
 
+    @DisplayName("object가 Map이면서 안에 foo 가 담겨있는지 확인하는 테스트")
     @Test
     void name() {
         Object object = Maps.newHashMap("foo", "anyValue");
