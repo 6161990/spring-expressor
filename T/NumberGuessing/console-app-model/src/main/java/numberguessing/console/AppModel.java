9547caebc09339fad187680d0439276f70985e45
Yoonji Kim <6161990src@gmail.com>
2022-03-23 07:13:25 +0900

    Test4:GREEN

--- T/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ T/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -5,6 +5,9 @@ import numberguessing.PositiveIntegerGenerator;
 public class AppModel {
 
     private static final String NEW_LINE = System.lineSeparator();
+    private String output = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
+            "3: Exit" + NEW_LINE + "Enter selection: ";
+
     private boolean completed;
 
     public AppModel(PositiveIntegerGenerator randomGenerator) {
@@ -16,13 +19,14 @@ public class AppModel {
     }
 
     public String flushOutput() {
-        return "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
-                "3: Exit" + NEW_LINE + "Enter selection: ";
+        return output;
     }
 
     public void processInput(String input) {
         if(input.equals("3")){
             completed = true;
+        } else if(input.equals("1")){
+            output = "Single player game Start!" + NEW_LINE + "I'm thinking of a number between 1 and 100." + NEW_LINE + "Enter your guess: ";
         }
     }
 }
