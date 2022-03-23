4bd785096be844b75e900f8e6427ccc5309994c3
Yoonji Kim <6161990src@gmail.com>
2022-03-24 06:31:49 +0900

    Test5:GREEN

--- T/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ T/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -7,14 +7,18 @@ public class AppModel {
     private static final String NEW_LINE = System.lineSeparator();
     private String output = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
             "3: Exit" + NEW_LINE + "Enter selection: ";
-
     private boolean completed;
+    private static int answer = 0;
+    private boolean isSinglePlayerMode = true;
+
 
     public AppModel(PositiveIntegerGenerator randomGenerator) {
+        answer = randomGenerator.generateLessThanOrEqualToHundread();
         completed = false;
     }
 
     public boolean isCompleted() {
+        isSinglePlayerMode = false;
         return completed;
     }
 
@@ -23,10 +27,16 @@ public class AppModel {
     }
 
     public void processInput(String input) {
-        if(input.equals("3")){
+        if (input.equals("3")) {
             completed = true;
-        } else if(input.equals("1")){
+            return;
+        } else if (input.equals("1")) {
             output = "Single player game Start!" + NEW_LINE + "I'm thinking of a number between 1 and 100." + NEW_LINE + "Enter your guess: ";
+            return;
+        }
+
+        if (Integer.parseInt(input) < answer) {
+            output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
         }
     }
 }
