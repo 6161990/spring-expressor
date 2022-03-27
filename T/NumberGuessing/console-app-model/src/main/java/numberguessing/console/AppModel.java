ac3a8764d90d737bd15c7c8cf3c9f38bc26bc9e2
Yoonji Kim <6161990src@gmail.com>
2022-03-28 08:59:05 +0900

    Test11:REFACTORING

--- T/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ T/NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -5,8 +5,9 @@ import numberguessing.PositiveIntegerGenerator;
 public class AppModel {
 
     private static final String NEW_LINE = System.lineSeparator();
-    private String output = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
+    private static final String SELECT_MODE_MESSAGE = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
             "3: Exit" + NEW_LINE + "Enter selection: ";
+    private String output;
     private boolean completed;
     private static int answer = 0;
     private boolean isSinglePlayerMode;
@@ -18,6 +19,7 @@ public class AppModel {
         completed = false;
         isSinglePlayerMode = false;
         tries=0;
+        output = SELECT_MODE_MESSAGE;
     }
 
     public boolean isCompleted() {
@@ -53,8 +55,7 @@ public class AppModel {
         } else if (Integer.parseInt(input) > answer) {
             output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
         } else {
-             output = "Correct! " + tries + (tries == 1? " guess."  :  " guesses." ) + NEW_LINE + "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE +
-                    "3: Exit" + NEW_LINE + "Enter selection: ";
+             output = "Correct! " + tries + (tries == 1? " guess."  :  " guesses." ) + NEW_LINE + SELECT_MODE_MESSAGE ;
         }
     }
 }
