6e0826fc74241601071a21a95c18a58c3ef48859
Yoonji Kim <6161990src@gmail.com>
2022-03-29 10:19:25 +0900

    NumberGuessingGame Round2 Start - SinglePlayer

---
+++ T/NumberGuessing/console-app-model/src/main/java/numberGuessing/console/AppModel.java
@@ -0,0 +1,20 @@
+package numberGuessing.console;
+
+import numberGuessing.PositiveIntegerGenerator;
+
+public class AppModel {
+
+    public AppModel(PositiveIntegerGenerator randomGenerator) {
+    }
+
+    public boolean isCompleted() {
+        return true;
+    }
+
+    public String flushOutput() {
+        return null;
+    }
+
+    public void processInput(String input) {
+    }
+}
