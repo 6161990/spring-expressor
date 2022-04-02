0f662ebf9ed76c9ff5f2b94ac0c817ffb4a64766
Yoonji Kim <6161990src@gmail.com>
2022-04-02 20:50:40 +0900

    Step19~20 Refactoring

--- T/NumberGuessing/console-app-model/src/main/java/numberGuessing/console/AppModel.java
+++ T/NumberGuessing/console-app-model/src/main/java/numberGuessing/console/AppModel.java
@@ -10,8 +10,6 @@ public class AppModel {
     private final PositiveIntegerGenerator randomGenerator;
     private boolean completed;
     private String output;
-    private static int answer = 0;
-    private boolean isSinglePlayerMode;
     private Processor processor;
 
     interface Processor {
@@ -23,7 +21,6 @@ public class AppModel {
         this.randomGenerator = randomGenerator;
         completed = false;
         output = SELECT_MODE_MESSAGE;
-        isSinglePlayerMode = false;
     }
 
     public boolean isCompleted() {
@@ -40,28 +37,26 @@ public class AppModel {
 
     private Processor processModeSelection(String input) {
         if(input.equals("1")){
-            isSinglePlayerMode = true;
-            answer = randomGenerator.generateLessThanEqualToHundred();
+            int answer = randomGenerator.generateLessThanEqualToHundred();
             output ="Single player game Start!" + NEW_LINE + "I'm thinking of a number between 1 and 100."
                     + NEW_LINE + "Enter your guess: ";
-            return getSinglePlayGameProcessor(1);
+            return getSinglePlayGameProcessor(1 , answer);
         } else {
             completed = true;
             return null;
         }
     }
 
-    private Processor getSinglePlayGameProcessor(int tries) {
+    private Processor getSinglePlayGameProcessor(int tries, int answer) {
         return input -> {
             if (Integer.parseInt(input) < answer) {
                 output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
-                return getSinglePlayGameProcessor(tries + 1);
+                return getSinglePlayGameProcessor(tries + 1, answer);
             } else if (Integer.parseInt(input) > answer) {
                 output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
-                return getSinglePlayGameProcessor(tries + 1);
+                return getSinglePlayGameProcessor(tries + 1, answer);
             } else {
                 output = "Correct! " + tries + (tries == 1 ? " guess." : " guesses.") + NEW_LINE + SELECT_MODE_MESSAGE;
-                isSinglePlayerMode = false;
                 return this::processModeSelection;
             }
         };
