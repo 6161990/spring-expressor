9d0d49597e9d1f7772cf8a457472da2b2d8c38e7
Yoonji Kim <6161990src@gmail.com>
2022-04-03 11:55:00 +0900

    Step 2 - Created PositiveIntegerGeneratorStub

---
+++ T/NumberGuessing/console-app-model/src/test/java/numberGuessing/PositiveIntegerGeneratorStub.java
@@ -0,0 +1,20 @@
+package numberGuessing;
+
+public class PositiveIntegerGeneratorStub implements PositiveIntegerGenerator {
+
+    private int[] numbers;
+    private int index;
+
+    public PositiveIntegerGeneratorStub(int... numbers) {
+        this.numbers = numbers;
+        index = 0;
+    }
+
+
+    @Override
+    public int generateLessThanEqualsToHundred() {
+        int number = numbers[index];
+        index = (index + 1) % numbers.length;
+        return number;
+    }
+}
