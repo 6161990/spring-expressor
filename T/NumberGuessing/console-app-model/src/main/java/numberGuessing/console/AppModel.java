73e35918e97a17132ea1c5ade82d0824c6913ee1
Yoonji Kim <6161990src@gmail.com>
2022-04-03 11:49:10 +0900

    Step 1 - Created App
     with AppModel , PositiveIntegerGenerator, RandomGenerator

---
+++ T/NumberGuessing/console-app-model/src/main/java/numberGuessing/console/AppModel.java
@@ -0,0 +1,20 @@
+package numberGuessing.console;
+
+import numberGuessing.PositiveIntegerGenerator;
+
+public class AppModel {
+
+    public AppModel(PositiveIntegerGenerator randomGenerator) {
+    }
+
+    public boolean isCompleted() {
+        return false;
+    }
+
+    public String flushOutput() {
+        return null;
+    }
+
+    public void processInput(String nextLine) {
+    }
+}
---
+++ T/NumberGuessing/console-app/src/main/java/numberGuessing/console/App.java
@@ -0,0 +1,22 @@
+package numberGuessing.console;
+
+import numberGuessing.RandomGenerator;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        AppModel appModel = new AppModel(new RandomGenerator());
+        Scanner scanner = new Scanner(System.in);
+        runLoop(appModel, scanner);
+        scanner.close();
+    }
+
+    private static void runLoop(AppModel appModel, Scanner scanner) {
+        while(appModel.isCompleted() == false) {
+            System.out.println(appModel.flushOutput());
+            appModel.processInput(scanner.nextLine());
+        }
+    }
+}
