623e9f32c1c045257d9a3ca3a7446afbefa64646
Yoonji Kim <6161990src@gmail.com>
2022-04-03 15:49:38 +0900

    Step 7 [Test8,9,10,11,12]

--- T/NumberGuessing/console-app-model/src/main/java/numberGuessing/console/AppModel.java
+++ T/NumberGuessing/console-app-model/src/main/java/numberGuessing/console/AppModel.java
@@ -4,19 +4,24 @@ import numberGuessing.PositiveIntegerGenerator;
 
 public class AppModel {
 
+    private final PositiveIntegerGenerator randomGenerator;
     private static final String NEW_LINE = System.lineSeparator();
-    private static final String SELECTION_MODE_MESSAGE = "1: Single player game" + NEW_LINE +
+    private static final String SELECT_MODE_MESSAGE = "1: Single player game" + NEW_LINE +
             "2: Multiplayer game" + NEW_LINE + "3: Exit" + NEW_LINE + "Enter selection: ";
     private static final String SINGLE_PLAYER_START_MESSAGE = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
                 + NEW_LINE + "Enter your guess: ";
     private boolean isCompleted;
-    private String output = SELECTION_MODE_MESSAGE;
+    private String output;
     private int answer;
     private boolean isSinglePlayerGameMode;
+    private int tries;
 
     public AppModel(PositiveIntegerGenerator randomGenerator) {
+        this.randomGenerator = randomGenerator;
         this.isCompleted = false;
-        answer = randomGenerator.generateLessThanEqualsToHundred();
+        output = SELECT_MODE_MESSAGE;
+        tries = 0;
+        answer = 0;
     }
 
     public boolean isCompleted() {
@@ -38,6 +43,7 @@ public class AppModel {
     private void selectionGameMode(String input) {
         if (input == "1") {
             isSinglePlayerGameMode = true;
+            answer = randomGenerator.generateLessThanEqualsToHundred();
             output = SINGLE_PLAYER_START_MESSAGE;
         } else {
             isCompleted = true;
@@ -45,12 +51,14 @@ public class AppModel {
     }
 
     private void singlePlayerMode(String input) {
+        tries++;
         if (Integer.parseInt(input) < answer) {
             output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
         } else if (Integer.parseInt(input) > answer) {
             output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
         } else if(Integer.parseInt(input) == answer) {
-            output = "Correct! " + NEW_LINE;
+            output = "Correct! + " + tries + (tries == 1 ? " guess." : " guesses." ) + NEW_LINE + SELECT_MODE_MESSAGE;
+            isSinglePlayerGameMode = false;
         }
     }
 }
