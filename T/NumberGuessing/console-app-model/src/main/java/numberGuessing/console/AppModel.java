755e066c0dc1bcc5d198db26fc61874187ef53d6
Yoonji Kim <6161990src@gmail.com>
2022-04-17 18:32:46 +0900

    Step16~Step20

--- T/NumberGuessing/console-app-model/src/main/java/numberGuessing/console/AppModel.java
+++ T/NumberGuessing/console-app-model/src/main/java/numberGuessing/console/AppModel.java
@@ -7,17 +7,20 @@ public class AppModel {
     public static final String NEW_LINE = System.lineSeparator();
     public static final String GAME_MODE_SELECT_MESSAGE = "1: Single player game" + NEW_LINE +
             "2: Multiplayer game" + NEW_LINE + "3: Exit" + NEW_LINE + "Enter selection: ";
+    private final PositiveIntegerGenerator randomGenerator;
     private boolean isCompleted;
     private String output;
-    private int answer;
-    private boolean isSingleGameProcess;
-    private int tries = 0;
+    private Processor processor;
+
+    interface Processor {
+        Processor run(String input);
+    }
 
     public AppModel(PositiveIntegerGenerator generator) {
+        randomGenerator = generator;
         isCompleted = false;
         output = GAME_MODE_SELECT_MESSAGE;
-        answer = generator.generateLessThanOrEqualToHundred();
-        isSingleGameProcess = false;
+        processor = this::selectMode;
     }
 
     public boolean isCompleted() {
@@ -29,33 +32,34 @@ public class AppModel {
     }
 
     public void processInput(String input) {
-        if(isSingleGameProcess) {
-            singleGameProcess(input);
-        } else {
-            selectMode(input);
-        }
+        processor = processor.run(input);
     }
 
-    private void selectMode(String input) {
+    private Processor selectMode(String input) {
         if(input == "1") {
+            int answer = randomGenerator.generateLessThanOrEqualToHundred();
             output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
                     + NEW_LINE + "Enter your guess: ";
-            isSingleGameProcess = true;
-        } else if(input == "3"){
+            return singleGameProcess(answer , 1);
+        } else {
             isCompleted = true;
+            return null;
         }
     }
 
-    private void singleGameProcess(String input) {
-        tries++;
-        if(Integer.parseInt(input) < answer) {
-            output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
-        } else if(Integer.parseInt(input) > answer) {
-            output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
-        } else {
-            output = "Correct! ";
-            output += tries + (tries == 1? " guess." : " guesses.") + NEW_LINE + GAME_MODE_SELECT_MESSAGE;
-            isSingleGameProcess = false;
-        }
+    private Processor singleGameProcess(int answer, int tries) {
+        return input -> {
+            if (Integer.parseInt(input) < answer) {
+                output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
+                return singleGameProcess(answer, tries + 1);
+            } else if (Integer.parseInt(input) > answer) {
+                output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
+                return singleGameProcess(answer, tries + 1);
+            } else {
+                output = "Correct! ";
+                output += tries + (tries == 1 ? " guess." : " guesses.") + NEW_LINE + GAME_MODE_SELECT_MESSAGE;
+                return this::selectMode;
+            }
+        };
     }
 }
