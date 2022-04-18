cba8cc6411a21c8101ca204cc97620b061e529fc
Yoonji Kim <6161990src@gmail.com>
2022-04-19 07:24:34 +0900

    StarkIndustries 사의 StarkIndustriesProductImporter, Source, Translator 생성

---
+++ T/ProductImporter/src/main/java/productImporter/suppliers/starkIndustries/StarkIndustriesProductImporter.java
@@ -0,0 +1,26 @@
+package productImporter.suppliers.starkIndustries;
+
+import productImporter.ProductImporter;
+import productImporter.Product;
+
+import java.util.ArrayList;
+
+public class StarkIndustriesProductImporter implements ProductImporter {
+
+    private final StarkIndustriesProductSource productSource;
+    private final StarkIndustriesProductTranslator translator;
+
+    public StarkIndustriesProductImporter(StarkIndustriesProductSource productSource, StarkIndustriesProductTranslator translator) {
+        this.productSource = productSource;
+        this.translator = translator;
+    }
+
+    @Override
+    public Iterable<Product> fetchProducts() {
+        var products = new ArrayList<Product>();
+        for(StarkIndustriesProduct s : productSource.getAllProducts()){
+            products.add(translator.translate(s));
+        }
+        return products;
+    }
+}
