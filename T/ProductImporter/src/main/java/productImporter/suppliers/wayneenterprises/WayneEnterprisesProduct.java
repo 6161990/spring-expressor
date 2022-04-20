537ee1dbeaa80a495cb9f5c76ff3b0907e8858fd
Yoonji Kim <6161990src@gmail.com>
2022-04-20 22:01:53 +0900

    WayneEnterprises

--- T/ProductImporter/src/main/java/productImporter/suppliers/wayneenterprises/WayneEnterprisesProduct.java
+++ T/ProductImporter/src/main/java/productImporter/suppliers/wayneenterprises/WayneEnterprisesProduct.java
@@ -1,4 +1,19 @@
 package productImporter.suppliers.wayneenterprises;
 
-public class WayneEnterprisesProduct {
+import lombok.Getter;
+
+@Getter
+public final class WayneEnterprisesProduct {
+    private final String id;
+    private final String title;
+    private final int listPrice;
+    private final int sellingPrice;
+
+    public WayneEnterprisesProduct(String id, String title, int listPrice, int sellingPrice) {
+        this.id = id;
+        this.title = title;
+        this.listPrice = listPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
 }
