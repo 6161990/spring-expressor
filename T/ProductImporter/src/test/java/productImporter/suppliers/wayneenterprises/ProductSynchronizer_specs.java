0c7fc1fdef5638b61fc0cca656285ff2c73b1770
Yoonji Kim <6161990src@gmail.com>
2022-04-25 08:54:41 +0900

    Create ProductSynchronizer_specs.java

---
+++ T/ProductImporter/src/test/java/productImporter/suppliers/wayneenterprises/ProductSynchronizer_specs.java
@@ -0,0 +1,28 @@
+package productImporter.suppliers.wayneenterprises;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import productImporter.*;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProductSynchronizer_specs {
+
+    @DisplayName("ProductSynchronizer 가 ProductInventory에 상품을 잘 저장한")
+    @ParameterizedTest
+    @DomainArgumentsSource
+    void sut_correctly_saves_products(WayneEnterprisesProduct[] products){
+        var stub = new WayneEnterprisesProductSourceStub(products);
+        var importer = new WayneEnterprisesProductImporter(stub);
+        var validator = new ListPriceFilter(BigDecimal.ZERO); // 특정 값 이상만 유효한 것으로 판단하는 필터(0이상의 값의 상품을 통과시킴)
+        var spy = new ProductInventorySpy();
+        var sut = new ProductSynchronizer(importer, validator, spy); 
+
+        sut.run();
+
+        Iterable<Product> expected = importer.fetchProducts();
+        assertThat(spy.getLog()).usingRecursiveFieldByFieldElementComparator();
+    }
+}
