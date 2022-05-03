c6bc24f0dc75f2a6721e86784e103c357d6b4e1b
Yoonji Kim <6161990src@gmail.com>
2022-05-04 08:11:14 +0900

    Step13-2-1

--- T/ProductImporter/src/test/java/productImporter/suppliers/wayneenterprises/WayneEnterprisesProductSourceStub.java
+++ T/ProductImporter/src/test/java/productImporter/suppliers/wayneenterprises/WayneEnterprisesProductSourceStub.java
@@ -3,6 +3,16 @@ package productImporter.suppliers.wayneenterprises;
 import java.util.Arrays;
 
 /** 입력받은 값을 그대로 갖고 있다가 호출될 때 그대로 반환 -> 준비된 값을 가지고 있는 셈*/
-public class WayneEnterprisesProductSourceStub  {
+public class WayneEnterprisesProductSourceStub implements WayneEnterprisesProductSource {
 
+    private WayneEnterprisesProduct[] products;
+
+    public WayneEnterprisesProductSourceStub(WayneEnterprisesProduct... products) {
+        this.products = products;
+    }
+
+    @Override
+    public Iterable<WayneEnterprisesProduct> fetchProducts() {
+        return Arrays.asList(products);
+    }
 }
