cff3e240b8fa4d78844c0a23ca07428ebce1fc69
Yoonji Kim <6161990src@gmail.com>
2022-05-05 10:06:52 +0900

    Step 18 All Clear, Step 19
    Step18. ProductSynchronizer_specs ✅
    * [Step18-1. ProductSynchronizer 가 ProductInventory 에 상품을 잘 저장한다.]
      Step18-1-2. ProductInventorySpy ✅
    * [Step18-2. 올바르지 않은 상품은 저장하지 않는다.]
    * [Step18-3. Spy 를 사용하던 ProductSynchronizer 를 Mock 을 사용해 TestCase 작성]
    Step19. ProductSynchronizer ✅

--- T/ProductImporter/src/test/java/productImporter/ProductInventorySpy.java
+++ T/ProductImporter/src/test/java/productImporter/ProductInventorySpy.java
@@ -2,6 +2,20 @@ package productImporter;
 
 import java.util.ArrayList;
 
-public class ProductInventorySpy {
+public class ProductInventorySpy implements ProductInventory{
 
+    private final ArrayList<Product> log;
+
+    public ProductInventorySpy() {
+        log = new ArrayList<Product>();
+    }
+
+    public ArrayList<Product> getLog() {
+        return log;
+    }
+
+    @Override
+    public void upsertProduct(Product product) {
+        log.add(product);
+    }
 }
