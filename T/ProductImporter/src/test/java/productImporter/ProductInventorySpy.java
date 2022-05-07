06b6134701d59042e088cc7ac24e605a3160bb74
Yoonji Kim <6161990src@gmail.com>
2022-05-08 08:36:46 +0900

    Step16 ~ Step19

--- T/ProductImporter/src/test/java/productImporter/ProductInventorySpy.java
+++ T/ProductImporter/src/test/java/productImporter/ProductInventorySpy.java
@@ -2,7 +2,20 @@ package productImporter;
 
 import java.util.ArrayList;
 
-public class ProductInventorySpy {
+public class ProductInventorySpy implements ProductInventory {
 
+    private final ArrayList<Product> log;
 
+    public ProductInventorySpy() {
+        log = new ArrayList<>();
+    }
+
+    public ArrayList<Product> getLog() {
+        return log;
+    }
+
+    @Override
+    public void upsertProduct(Product product) {
+        log.add(product);
+    }
 }
--- T/ProductImporter/src/test/java/productImporter/ProductSynchronizer_specs.java
+++ T/ProductImporter/src/test/java/productImporter/ProductSynchronizer_specs.java
@@ -2,6 +2,8 @@ package productImporter;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import productImporter.suppliers.wayneenterprises.WayneEnterprisesProduct;
+import productImporter.suppliers.wayneenterprises.WayneEnterprisesProductImporter;
 import productImporter.suppliers.wayneenterprises.WayneEnterprisesProductSourceStub;
 
 import java.math.BigDecimal;
@@ -14,7 +16,18 @@ public class ProductSynchronizer_specs {
 
     @DisplayName("ProductSynchronizer 가 ProductInventory 에 상품을 잘 저장한다.")
     @ParameterizedTest
-    void sut_correctly_saves_products() {
+    @DomainArgumentsSource
+    void sut_correctly_saves_products(WayneEnterprisesProduct[] products) {
+        var stub = new WayneEnterprisesProductSourceStub(products);
+        var importer = new WayneEnterprisesProductImporter(stub);
+        var validator = new ListPriceFilter(BigDecimal.ZERO);
+        var spy = new ProductInventorySpy();
+        var sut = new ProductSynchronizer(importer, validator, spy);
+
+        sut.run();
+
+        Iterable<Product> expected = importer.fetchProducts();
+        assertThat(spy.getLog()).usingRecursiveFieldByFieldElementComparator().containsAll(expected);
 
 
         /**
@@ -24,14 +37,42 @@ public class ProductSynchronizer_specs {
 
     @DisplayName("올바르지 않은 상품은 저장하지 않는다.")
     @ParameterizedTest
-    void sut_does_not_save_invalid_product( ) {
+    @DomainArgumentsSource
+    void sut_does_not_save_invalid_product(WayneEnterprisesProduct product) {
+        var lowerBound = new BigDecimal(product.getListPrice() + 10000);
+        var validator = new ListPriceFilter(lowerBound);
 
+        var stub = new WayneEnterprisesProductSourceStub(product);
+        var importer = new WayneEnterprisesProductImporter(stub);
+        var spy = new ProductInventorySpy();
+        var sut = new ProductSynchronizer(importer, validator, spy);
+
+        sut.run();
+
+        assertThat(spy.getLog()).isEmpty();
     }
 
 
     @DisplayName("Spy 를 사용하던 ProductSynchronizer 를 Mock 을 사용한다.")
     @ParameterizedTest
+    @DomainArgumentsSource
     void sut_really_does_not_save_invalid_product() {
+        var pricing = new Pricing(BigDecimal.TEN, BigDecimal.ZERO);
+        var product = new Product("supplierName", "productCode", "productName", pricing);
+
+        ProductImporter importer = mock(ProductImporter.class);
+        when(importer.fetchProducts()).thenReturn(Arrays.asList(product));
+
+        var validator = mock(ProductValidator.class);
+        when(validator.isValid(product)).thenReturn(false);
+
+        ProductInventory inventory = mock(ProductInventory.class);
+
+        var sut = new ProductSynchronizer(importer, validator, inventory);
+
+        sut.run();
+
+        verify(inventory, never()).upsertProduct(product);
     }
 }
 
