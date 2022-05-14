05571d3179187c1365eb3ffd32770d9affd2665a
Yoonji Kim <6161990src@gmail.com>
2022-05-14 11:42:07 +0900

    인터페이스 기반으로 프락시를 생성하는  JDK 동적 프락시의 구체적 클래스 타입 test 하는 방법

--- Spring4_MovieBuddy/lib/src/test/java/moviebuddy/data/XmlMovieReaderTest.java
+++ Spring4_MovieBuddy/lib/src/test/java/moviebuddy/data/XmlMovieReaderTest.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.util.AopTestUtils;
 
 import moviebuddy.MovieBuddyFactory;
 import moviebuddy.MovieBuddyProfile;
 import moviebuddy.domain.Movie;
+import moviebuddy.domain.MovieReader;
 
 @SpringJUnitConfig(MovieBuddyFactory.class) // 위 둘을 한꺼번에 해결해주는 마법사 
 @ActiveProfiles(MovieBuddyProfile.XML_MODE)
@@ -19,12 +22,20 @@ import moviebuddy.domain.Movie;
 public class XmlMovieReaderTest {
 	
 	@Autowired
-	XmlMovieReader movieReader;  //구체적인 타입으로 의존 관계로 주입받음. MovieReader로 받으면 동일한 타입의 빈이 있어서 테스트 실패 
+	MovieReader movieReader;  //구체적인 타입으로 의존 관계로 주입받음. MovieReader로 받으면 동일한 타입의 빈이 있어서 테스트 실패 
 	
 	@Test
 	void NotEmpty_LoadedMovies() {
 		List<Movie> movies = movieReader.loadMovies();
 		Assertions.assertEquals(1375, movies.size());
+	}
+	
+	@Test
+	void Check_MovieReaderType() {
+		Assertions.assertTrue(AopUtils.isAopProxy(movieReader));
+		
+		MovieReader target = AopTestUtils.getTargetObject(movieReader);
+		Assertions.assertTrue(XmlMovieReader.class.isAssignableFrom(target.getClass()));
 	}
 
 }
