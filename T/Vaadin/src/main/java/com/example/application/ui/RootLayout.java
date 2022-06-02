cdee1ad9e855713daf1a53a5f59a6d375af80bfc
Yoonji Kim <6161990src@gmail.com>
2022-06-03 07:59:07 +0900

    Update RootLayout.java

--- T/Vaadin/src/main/java/com/example/application/ui/RootLayout.java
+++ T/Vaadin/src/main/java/com/example/application/ui/RootLayout.java
@@ -5,39 +5,27 @@ import com.example.application.ui.user.UserView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.html.*;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.RouterLink;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class RootLayout extends AppLayout {
-
-    private SecurityService securityService;
-
     public static class MenuItemInfo extends ListItem {
-
         private final Class<? extends Component> view;
-
         public MenuItemInfo(String menuTitle, String iconClass, Class<? extends Component> view) {
             this.view = view;
             RouterLink link = new RouterLink();
             link.addClassNames("menu-item-link");
             link.setRoute(view);
-
             Span text = new Span(menuTitle);
             text.addClassNames("menu-item-text");
-
             link.add(new LineAwesomeIcon(iconClass), text);
             add(link);
         }
-
         public Class<?> getView() {
             return view;
         }
-
         /**
          * Simple wrapper to create icons using LineAwesome iconset. See
          * https://icons8.com/line-awesome
@@ -51,61 +39,35 @@ public class RootLayout extends AppLayout {
                 }
             }
         }
-
     }
-
     private H1 viewTitle;
-
-    public RootLayout(@Autowired SecurityService securityService) {
-        this.securityService = securityService;
-
-        H1 logo = new H1("Vaadin CRM");
-        logo.addClassName("logo");
-        HorizontalLayout header;
-        if (securityService.getAuthenticatedUser() != null) {
-            Button logout = new Button("Logout", click ->
-                    securityService.logout());
-            header = new HorizontalLayout(logo, logout);
-        } else {
-            header = new HorizontalLayout(logo);
-        }
-
-
+    public RootLayout() {
         setPrimarySection(Section.DRAWER);
-        addToNavbar(header);
+        addToNavbar(true, createHeaderContent());
         addToDrawer(createDrawerContent());
     }
-
     private Component createDrawerContent() {
         H2 appName = new H2("NEW ADMIN");
         appName.addClassNames("app-name");
-
         com.vaadin.flow.component.html.Section section = new com.vaadin.flow.component.html.Section(appName,
                 createNavigation(), createFooter());
         section.addClassNames("drawer-section");
         return section;
     }
-
     private Component createFooter() {
         Footer layout = new Footer();
         layout.addClassNames("footer");
-
         return layout;
     }
-
     private Component createNavigation() {
         Nav nav = new Nav();
         nav.addClassNames("menu-item-container");
         nav.getElement().setAttribute("aria-labelledby", "views");
-
-
         UnorderedList list = new UnorderedList();
         list.addClassNames("navigation-list");
         nav.add(list);
-
         for (MenuItemInfo menuItem : createMenuItems()) {
             list.add(menuItem);
-
         }
         return nav;
     }
@@ -116,21 +78,15 @@ public class RootLayout extends AppLayout {
                 new MenuItemInfo("user", "la la-user", UserView.class),
         };
     }
-
     private Component createHeaderContent() {
         DrawerToggle toggle = new DrawerToggle();
         toggle.addClassName("layout-toggle");
         toggle.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
         toggle.getElement().setAttribute("aria-label", "Toggle Menu");
-
         viewTitle = new H1();
         viewTitle.addClassName("view-title");
-
         Header header = new Header(toggle, viewTitle);
         header.addClassNames("header");
-
         return header;
     }
-
-
 }
