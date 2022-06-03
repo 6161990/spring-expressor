e7eda1794e066ed6a71f451e8730456c34e6ab70
Yoonji Kim <6161990src@gmail.com>
2022-06-04 08:17:26 +0900

    Create Constants.java

---
+++ T/Vaadin/src/main/java/com/example/application/data/service/Constants.java
@@ -0,0 +1,10 @@
+package com.example.application.data.service;
+
+public final class Constants {
+
+    static final String VERSION = "v";
+    static final String FUNCTION = "f";
+    static final String V_0_1_0 = "0.1.0";
+    static final String FIND = "find";
+
+}
