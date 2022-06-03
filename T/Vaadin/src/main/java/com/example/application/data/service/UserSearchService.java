380f5c42d04684ebf45a5957f146c392819afda3
Yoonji Kim <6161990src@gmail.com>
2022-06-04 08:17:38 +0900

    Update UserSearchService.java

--- T/Vaadin/src/main/java/com/example/application/data/service/UserSearchService.java
+++ T/Vaadin/src/main/java/com/example/application/data/service/UserSearchService.java
@@ -1,53 +1,45 @@
 package com.example.application.data.service;
 
 import com.example.application.data.views.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+import static com.example.application.data.service.Constants.*;
 import static org.springframework.http.HttpMethod.GET;
 
 
 @Service
 public class UserSearchService {
 
-    private final RestTemplate restTemplate;
-    private static final String V = "v";
-    private static final String _0_1_0 = "0.1.0";
-    private static final String F = "f";
-    private static final String FIND = "find";
+    @Autowired
+    public RestTemplate restTemplate;
 
     @Value("${gateway.api.url}")
     private String gatewayApiUrl;
-
-    public UserSearchService(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
-    }
 
     public User getUserById(String userId) {
         UriComponents uri = UriComponentsBuilder.fromHttpUrl(gatewayApiUrl)
                 .path("/")
                 .path(userId)
-                .queryParam(V, _0_1_0)
+                .queryParam(VERSION, V_0_1_0)
                 .build();
 
-        ResponseEntity<User> response = restTemplate.exchange(uri.toUriString(), GET, getHttpEntity(), User.class);
+        ResponseEntity<User> response =  restTemplate.exchange(uri.toUriString(), GET, getHttpEntity(), User.class);
 
         return response.getBody();
     }
 
-    public List<User> getUserByName(String name)  {
+    public List<User> getUserByName(String name) {
         UriComponents uri = getUriCommonBuilder()
                 .queryParam("name", name)
                 .build();
@@ -71,8 +63,8 @@ public class UserSearchService {
 
     private UriComponentsBuilder getUriCommonBuilder() {
         return UriComponentsBuilder.fromHttpUrl(gatewayApiUrl)
-                .queryParam(V, _0_1_0)
-                .queryParam(F, FIND);
+                .queryParam(VERSION, V_0_1_0)
+                .queryParam(FUNCTION, FIND);
     }
 
     private HttpEntity getHttpEntity() {
