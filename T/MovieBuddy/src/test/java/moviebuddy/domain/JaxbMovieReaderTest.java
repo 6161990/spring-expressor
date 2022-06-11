2bc6ee4a7f888d5f65a2a8207d3397752373efc9
Yoonji Kim <6161990src@gmail.com>
2022-06-11 10:51:36 +0900

    [SECTION 03-2. JUnit Test]

--- T/MovieBuddy/src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
+++ T/MovieBuddy/src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -1,13 +1,18 @@
 package moviebuddy.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.List;
 
 class JaxbMovieReaderTest {
 
-    public static void main(String[] args) {
+    @Test
+    void NotEmpty_LoadedMovies(){
         JaxbMovieReader movieReader = new JaxbMovieReader();
 
         List<Movie> movies = movieReader.loadMovies();
-        MovieFinderTest.assertEquals(1375, movies.size());
+        Assertions.assertEquals(1375, movies.size());
     }
+
 }
