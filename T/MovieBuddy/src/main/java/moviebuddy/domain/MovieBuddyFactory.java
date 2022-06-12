1d52fbe015b3b17e2a58d25c2597f72b7b4bd1f2
Yoonji Kim <6161990src@gmail.com>
2022-06-13 08:29:01 +0900

    [SECTION 06-1. 의존관계주입 방식, 빈 스코프]

--- T/MovieBuddy/src/main/java/moviebuddy/domain/MovieBuddyFactory.java
+++ T/MovieBuddy/src/main/java/moviebuddy/domain/MovieBuddyFactory.java
@@ -1,13 +1,29 @@
 package moviebuddy.domain;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MovieBuddyFactory {
 
     @Bean
-    public MovieFinder movieFinder() {
-        return new MovieFinder(new CsvMovieReader());
+    public MovieReader movieReader(){
+        return new CsvMovieReader();
     }
+
+    @Bean // 의존 관계 주입 - 메소드 콜 방식
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public MovieFinder movieFinder() {
+        return new MovieFinder(movieReader());
+    }
+
+    /**
+     *   @Bean // 의존 관계 주입 - 메소드 파라미터 방식
+     *   public MovieFinder movieFinder(MovieReader movieReader) {
+     *       return new MovieFinder(movieReader);
+     *   }
+     * */
+
 }
