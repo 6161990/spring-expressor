5cea378380022af79d5d1c87224fd57383c7bf22
Yoonji Kim <6161990src@gmail.com>
2022-06-13 08:15:12 +0900

    [SECTION 05. 스프링 컨테이너 IoC, Bean]

--- T/MovieBuddy/src/main/java/moviebuddy/MovieBuddyApplication.java
+++ T/MovieBuddy/src/main/java/moviebuddy/MovieBuddyApplication.java
@@ -17,6 +17,8 @@ import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.Movie;
 import moviebuddy.domain.MovieBuddyFactory;
 import moviebuddy.domain.MovieFinder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MovieBuddyApplication {
 
@@ -34,8 +36,8 @@ public class MovieBuddyApplication {
      */
 
     public void run(String[] args) throws Exception {
-        final MovieBuddyFactory movieBuddyFactory = new MovieBuddyFactory();
-        final MovieFinder movieFinder = movieBuddyFactory.movieFinder();
+        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
+        final MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
         final AtomicBoolean running = new AtomicBoolean(true);
         final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         final PrintWriter output = new PrintWriter(System.out, false);
--- T/MovieBuddy/src/main/java/moviebuddy/domain/MovieBuddyFactory.java
+++ T/MovieBuddy/src/main/java/moviebuddy/domain/MovieBuddyFactory.java
@@ -1,7 +1,12 @@
 package moviebuddy.domain;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MovieBuddyFactory {
 
+    @Bean
     public MovieFinder movieFinder() {
         return new MovieFinder(new CsvMovieReader());
     }
