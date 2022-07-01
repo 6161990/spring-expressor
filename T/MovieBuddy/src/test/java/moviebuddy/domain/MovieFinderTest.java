1b35839ffd29ed04696d5bc69ac46b7cae669387
Yoonji Kim <6161990src@gmail.com>
2022-07-01 11:29:38 +0900

    SECTION 08. 스프링 통합 테스트 지원, 스프링 테스트 컨텍스트 프레임워크

--- T/MovieBuddy/src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ T/MovieBuddy/src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -1,29 +1,31 @@
 package moviebuddy.domain;
 
 import moviebuddy.MovieBuddyFactory;
-import moviebuddy.domain.Movie;
-import moviebuddy.domain.MovieFinder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = MovieBuddyFactory.class)
 class MovieFinderTest {
 
-	final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
-	final MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
+	@Autowired
+	MovieFinder sut;
 
 	@Test
 	void NotEmpty_directedBy() {
-		List<Movie> result = movieFinder.directedBy("Michael Bay");
+		List<Movie> result = sut.directedBy("Michael Bay");
 		Assertions.assertEquals(3, result.size());
 	}
 
 	@Test
 	void NotEmpty_ReleasedYearBy() {
-		List<Movie> result = movieFinder.releasedYearBy(2015);
+		List<Movie> result = sut.releasedYearBy(2015);
 		Assertions.assertEquals(225, result.size());
 	}
 	
