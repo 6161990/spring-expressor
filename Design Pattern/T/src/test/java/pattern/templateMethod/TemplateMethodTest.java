3350251ef1c760328c3d6e1bfe8bd2ec47ea5d4b
Yoonji Kim <6161990src@gmail.com>
2022-07-11 07:50:40 +0900

    클래스 명 변경

--- Design Pattern/T/src/test/java/pattern/templateMethod/TemplateMethodTest.java
+++ Design Pattern/T/src/test/java/pattern/templateMethod/TemplateMethodTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class TemplateMethod {
+public class TemplateMethodTest {
 
     Player player;
 
