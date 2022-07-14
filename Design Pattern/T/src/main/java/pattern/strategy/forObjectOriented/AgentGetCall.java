f93d189b8c05f8128cf69ef507e084002bfb6815
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-07-15 08:37:13 +0900

    추후 ConcreteStrategy 추가

--- Design Pattern/T/src/main/java/pattern/strategy/forObjectOriented/AgentGetCall.java
+++ Design Pattern/T/src/main/java/pattern/strategy/forObjectOriented/AgentGetCall.java
@@ -1,7 +1,15 @@
 package pattern.strategy.forObjectOriented;
 
 /** ConcreteStrategy : Strategy 에 선언된 여러 기능들을 구현 */
-public class AgentGetCall {
+public class AgentGetCall implements Scheduler{
+    @Override
+    public String getNextCall() {
+        return "agent 에서 고객의 전화를 가져옵니다.";
+    }
 
-    /** 필요하면 여기를 또 나중에 구현해서 Context if 문에 추가하면 된다*/
+    @Override
+    public String sendCallToAgent() {
+        return "고객의 전화를 넘겨줍니다.";
+    }
+
 }
