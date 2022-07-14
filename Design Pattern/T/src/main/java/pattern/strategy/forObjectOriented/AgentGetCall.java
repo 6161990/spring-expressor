d4ea5fcd153f988504e68000e915333c19c36e39
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-07-14 16:44:54 +0900

    Context

---
+++ Design Pattern/T/src/main/java/pattern/strategy/forObjectOriented/AgentGetCall.java
@@ -0,0 +1,7 @@
+package pattern.strategy.forObjectOriented;
+
+/** ConcreteStrategy : Strategy 에 선언된 여러 기능들을 구현 */
+public class AgentGetCall {
+
+    /** 필요하면 여기를 또 나중에 구현해서 Context if 문에 추가하면 된다*/
+}
